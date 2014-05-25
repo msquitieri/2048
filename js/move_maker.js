b075728602d4d2ab46f91365114a6333ca97670e
squit <michael.squitieri@gmail.com>
2014-05-25 01:27:50 -0400

    added global constants for directions + changed name to moveInDirection

--- js/move_maker.js
+++ js/move_maker.js
@@ -1,18 +1,23 @@
-function swipeInDirection(direction) {
+var NORTH = 0,
+    EAST  = 1,
+    SOUTH = 2,
+    WEST  = 3;
+
+function moveInDirection(direction) {
 	gameManager.inputManager.emit("move", direction);
 }
 
-function swipeNorth() {
-	swipeInDirection(0);
+function moveNorth() {
+	moveInDirection(NORTH);
 }
-function swipeSouth() {
-	swipeInDirection(2);
+function moveSouth() {
+	moveInDirection(SOUTH);
 }
-function swipeEast() {
-	swipeInDirection(1);
+function moveEast() {
+	moveInDirection(EAST);
 }
-function swipeWest() {
-	swipeInDirection(3);
+function moveWest() {
+	moveInDirection(WEST);
 }
 
 var randomIntervalObj;
@@ -20,7 +25,7 @@ function playRandomly(millis) {
   var timePerMoveInMilliseconds = millis || 125;
   randomIntervalObj = setInterval(function() {
     var move = Math.floor(Math.random() * 3);
-    swipeInDirection(move);
+    moveInDirection(move);
   }, timePerMoveInMilliseconds);
 }
 
