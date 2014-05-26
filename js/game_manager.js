6beed614e29a9d2aead82a63732b57dc1af0b38f
squit <michael.squitieri@gmail.com>
2014-05-26 17:13:39 -0400

    added utility function for each direction

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,7 +167,7 @@ GameManager.prototype.canMoveInDirection = function(direction) {
         } else {
           // We can update the tile, which is reset
           // but we can't update the grid!
-          
+
           // self.moveTile(tile, positions.farthest);
           tile.updatePosition(positions.farthest);
         }
@@ -181,6 +181,67 @@ GameManager.prototype.canMoveInDirection = function(direction) {
     });
   });
   return moved;
+};
+
+GameManager.prototype.getUtilityForDirection = function(direction) {
+  var self = this;
+
+  var cell, tile;
+
+  var vector     = this.getVector(direction);
+  var traversals = this.buildTraversals(vector);
+  var moved      = false;
+
+  var added      = 0;
+
+  // Save the current tile positions and remove merger information
+  this.prepareTiles();
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        // Only one merger per row traversal?
+        if (next && next.value === tile.value && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value * 2);
+          merged.mergedFrom = [tile, next];
+
+          added += merged.value;
+
+          // self.grid.insertTile(merged);
+          // self.grid.removeTile(tile);
+
+          // Converge the two tiles' positions
+          tile.updatePosition(positions.next);
+
+          // Update the score
+          // self.score += merged.value;
+
+          // The mighty 2048 tile
+          // if (merged.value === 2048) self.won = true;
+        } else {
+          // We can update the tile, which is reset
+          // but we can't update the grid!
+
+          // self.moveTile(tile, positions.farthest);
+          tile.updatePosition(positions.farthest);
+        }
+
+        if (!self.positionsEqual(cell, tile)) {
+          moved = true; // The tile moved from its original cell!
+        }
+
+        tile.resetPosition();
+      }
+    });
+  });
+  return added;
 };
 
 // Move tiles on the grid in the specified direction
