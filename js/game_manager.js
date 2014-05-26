f7595479f33f222e49e561317038ba7d65f81d62
squit <michael.squitieri@gmail.com>
2014-05-26 16:54:09 -0400

    added canMoveInDirection method to game_manager in order to see if a user can move in a given direction

--- js/game_manager.js
+++ js/game_manager.js
@@ -126,6 +126,63 @@ GameManager.prototype.moveTile = function (tile, cell) {
   tile.updatePosition(cell);
 };
 
+GameManager.prototype.canMoveInDirection = function(direction) {
+  var self = this;
+
+  var cell, tile;
+
+  var vector     = this.getVector(direction);
+  var traversals = this.buildTraversals(vector);
+  var moved      = false;
+
+  // Save the current tile positions and remove merger information
+  this.prepareTiles();
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        // Only one merger per row traversal?
+        if (next && next.value === tile.value && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value * 2);
+          merged.mergedFrom = [tile, next];
+
+          // self.grid.insertTile(merged);
+          // self.grid.removeTile(tile);
+
+          // Converge the two tiles' positions
+          tile.updatePosition(positions.next);
+
+          // Update the score
+          // self.score += merged.value;
+
+          // The mighty 2048 tile
+          // if (merged.value === 2048) self.won = true;
+        } else {
+          // We can update the tile, which is reset
+          // but we can't update the grid!
+          
+          // self.moveTile(tile, positions.farthest);
+          tile.updatePosition(positions.farthest);
+        }
+
+        if (!self.positionsEqual(cell, tile)) {
+          moved = true; // The tile moved from its original cell!
+        }
+
+        tile.resetPosition();
+      }
+    });
+  });
+  return moved;
+};
+
 // Move tiles on the grid in the specified direction
 GameManager.prototype.move = function (direction) {
   // 0: up, 1: right, 2: down, 3: left
