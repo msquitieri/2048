9f600cf958b77629d5b63193dd81de182b4d80e1
squit <michael.squitieri@gmail.com>
2014-05-26 16:54:54 -0400

    can now log runs to rails + keeps track of number of moves

--- js/move_maker.js
+++ js/move_maker.js
@@ -8,6 +8,7 @@ function moveInDirection(direction) {
 }
 
 function restartGame() {
+  moves = 0;
   gameManager.inputManager.emit("restart");
 }
 
@@ -24,16 +25,51 @@ function moveWest() {
 	moveInDirection(WEST);
 }
 
+function logRun (type, score, moves, largestNumber) {
+  var run = {};
+  run.run_type = type;
+  run.score = score;
+  run.moves = moves;
+  run.largest_number = largestNumber;
+
+  console.log("LOGGING RUN!!!");
+
+  $.ajax({
+    url: "/runs.json",
+    method : "POST",
+    dataType : "json",
+    data : { "run" : run },
+    success : function () {
+      console.log("success!!");
+    },
+    error : function () {
+      console.log("error!!!");
+    }
+  });
+}
 // It performed much better when
 // you could not move west!
 var randomIntervalObj;
+var moves = 0;
 function playRandomly(millis) {
+  playRandomlyWithoutDirection(null);
+}
+
+function playRandomlyWithoutDirection(direction, millis) {
   var timePerMoveInMilliseconds = millis || 125;
   randomIntervalObj = setInterval(function() {
-    // if (gameManager.over) restartGame();
+    if (gameManager.over) {
+      logRun("random", gameManager.score, moves, gameManager.grid.largestNumber());
+      console.log("score : " + gameManager.score 
+            + " ; moves : " + moves + " ; largestNumber : " + gameManager.grid.largestNumber());
+      restartGame();
+    }
 
     var move = Math.floor(Math.random() * 4);
+    while (move == direction) move = Math.floor(Math.random() * 4);
     moveInDirection(move);
+    // TODO: Only increment move if it is a valid move!
+    moves++;
   }, timePerMoveInMilliseconds);
 }
 
